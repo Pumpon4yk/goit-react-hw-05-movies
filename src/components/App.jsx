1b94a08beee3802bed478ad8a1ee2734472b8469
Pumpon4yk <tt10392@gmail.com>
2023-03-12 04:36:57 +0100

    test3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,6 @@ const Movies = lazy(() => import('../pages/Movies'));
 const MovieDatalis = lazy(() => import('../pages/MovieDetails'));
 const Cast = lazy(() => import('./Cast/Cast'));
 const Reviews = lazy(() => import('./Reviews/Reviews'));
-const NotFound = lazy(() => import('./NotFound/NotFound'));
 
 export const App = () => {
     return (
@@ -19,7 +18,7 @@ export const App = () => {
                     <Route path="cast" element={<Cast />} />
                     <Route path="reviews" element={<Reviews />} />
                 </Route>
-                <Route path="*" element={<NotFound />} />
+                <Route path="*" element={<Home />} />
             </Route>
         </Routes>
     );
